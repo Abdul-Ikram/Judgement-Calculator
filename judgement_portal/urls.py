87359bc82beb423b0c30211021e16c49e4aa2691
Abdul-Ikram <abdulikram202@gmail.com>
2025-07-14 22:28:48 +0500

    fix: Deployment issue

--- judgement_portal/urls.py
+++ judgement_portal/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('authentication.urls')),
     path('authentication/api/', include('authentication.urls')),
     path('docket/api/', include('docket.urls')),
 ]
