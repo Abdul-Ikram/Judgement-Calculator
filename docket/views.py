c34adc291fc0c4d74f9a59daa9819536ba2b897a
Abdul Ikram <92265454+Abdul-Ikram@users.noreply.github.com>
2025-07-20 10:52:19 -0700

    update: error response for internal server

--- docket/views.py
+++ docket/views.py
@@ -68,7 +68,8 @@ class AddCaseView(APIView):
             except Exception as e:
                 return Response({
                     'status_code': 500,
-                    'message': f'Error creating case: {str(e)}'
+                    'message': f'Internal Server Error'
+                    # 'message': f'Error creating case: {str(e)}'
                 }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         return Response({
@@ -156,7 +157,8 @@ class CreateTransactionView(APIView):
             except Exception as e:
                 return Response({
                     'status_code': 500,
-                    'message': f"An error occurred: {str(e)}"
+                    # 'message': f"An error occurred: {str(e)}"
+                    'message': f'Internal Server Error'
                 }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         return Response({
